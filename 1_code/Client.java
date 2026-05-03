272290d270154c4d6b5263a42f21e3578b6eb148
wattsra129 <wattsra@iu.edu>
2026-05-03 19:53:53 -0400

    Add Client class with constructor and methods

--- 1_code/Client.java
+++ 1_code/Client.java
@@ -1,1 +1,21 @@
 
+public class Client {
+    private int clientID;
+    private String firstName;
+    private String lastName;
+
+    public Client(int clientID, String firstName, String lastName) {
+        this.clientID = clientID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
+
