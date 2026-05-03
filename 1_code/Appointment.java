721f2f816496b848c4977fb1d65239bf521531d4
wattsra129 <wattsra@iu.edu>
2026-05-03 19:56:01 -0400

    Add Appointment class with display method

--- 1_code/Appointment.java
+++ 1_code/Appointment.java
@@ -1,1 +1,25 @@
 
+public class Appointment {
+    private Client client;
+    private Provider provider;
+    private Service service;
+    private String appointmentDay;
+    private String appointmentTime;
+
+    public Appointment(Client client, Provider provider, Service service, String appointmentDay, String appointmentTime) {
+        this.client = client;
+        this.provider = provider;
+        this.service = service;
+        this.appointmentDay = appointmentDay;
+        this.appointmentTime = appointmentTime;
+    }
+
+    public void displayAppointment() {
+        System.out.println("Appointment Scheduled:");
+        System.out.println("Client: " + client.getFullName());
+        System.out.println("Provider: " + provider.getFullName());
+        System.out.println("Service: " + service.getServiceName());
+        System.out.println("Day: " + appointmentDay);
+        System.out.println("Time: " + appointmentTime);
+    }
+}
