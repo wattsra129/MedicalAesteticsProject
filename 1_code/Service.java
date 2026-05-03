5bf385d9e79a62c2954a5f7ecd40722702a23d68
wattsra129 <wattsra@iu.edu>
2026-05-03 19:54:48 -0400

    Add Service class with properties and constructor

--- 1_code/Service.java
+++ 1_code/Service.java
@@ -1,1 +1,24 @@
 
+public class Service {
+    private int serviceID;
+    private String serviceName;
+    private String requiredCertification;
+
+    public Service(int serviceID, String serviceName, String requiredCertification) {
+        this.serviceID = serviceID;
+        this.serviceName = serviceName;
+        this.requiredCertification = requiredCertification;
+    }
+
+    public int getServiceID() {
+        return serviceID;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getRequiredCertification() {
+        return requiredCertification;
+    }
+}
