ce984bca29fa641d146aca4c09d34c863e9dcbf0
wattsra129 <wattsra@iu.edu>
2026-05-03 19:54:21 -0400

    Add Provider class with attributes and methods

--- 1_code/Provider.java
+++ 1_code/Provider.java
@@ -1,1 +1,32 @@
 
+public class Provider {
+    private int providerID;
+    private String firstName;
+    private String lastName;
+    private String certification;
+    private String availableDay;
+
+    public Provider(int providerID, String firstName, String lastName, String certification, String availableDay) {
+        this.providerID = providerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.certification = certification;
+        this.availableDay = availableDay;
+    }
+
+    public int getProviderID() {
+        return providerID;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getCertification() {
+        return certification;
+    }
+
+    public String getAvailableDay() {
+        return availableDay;
+    }
+}
