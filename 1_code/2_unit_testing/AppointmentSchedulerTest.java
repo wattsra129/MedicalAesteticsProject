2b02fe44fd89f7e253a9b8ba38b7e099cfaa5536
wattsra129 <wattsra@iu.edu>
2026-05-03 20:04:59 -0400

    Add unit tests for AppointmentScheduler

--- 1_code/2_unit_testing/AppointmentSchedulerTest.java
+++ 1_code/2_unit_testing/AppointmentSchedulerTest.java
@@ -1,1 +1,24 @@
 
+public class AppointmentSchedulerTest {
+    public static void main(String[] args) {
+
+        AppointmentScheduler scheduler = new AppointmentScheduler();
+
+        Provider provider = new Provider(2001, "Lindsey", "Bruscia", "Esthetician", "Monday");
+        Service service = new Service(3001, "HydraFacial", "Esthetician");
+
+        boolean certifiedResult = scheduler.isProviderCertified(provider, service);
+        boolean availableResult = scheduler.isProviderAvailable(provider, "Monday");
+
+        System.out.println("Unit Test 1 - Provider Certification Check:");
+        System.out.println("Expected: true");
+        System.out.println("Actual: " + certifiedResult);
+
+        System.out.println();
+
+        System.out.println("Unit Test 2 - Provider Availability Check:");
+        System.out.println("Expected: true");
+        System.out.println("Actual: " + availableResult);
+    }
+}
+
